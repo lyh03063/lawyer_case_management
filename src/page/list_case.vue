d656a2183bbb1c273ca6a41f870ca4f29de6037f
lyh <113381161@qq.com>
2019-08-08 20:24:28 +0800

    lyh

--- src/page/list_case.vue
+++ src/page/list_case.vue
@@ -21,198 +21,212 @@ export default {
           list: "/crossList?page=lawyer_case", //列表接口
           add: "/crossAdd?page=lawyer_case", //新增接口
           modify: "/crossModify?page=lawyer_case", //修改接口
+          detail: "/crossDetail?page=lawyer_case", //详情接口
           delete: "/crossDelete?page=lawyer_case" //删除接口
         },
         //-------列配置数组-------
         columns: [
           {
-            label: "会员号",
+            label: "Id",
             prop: "P1",
             width: 80
           },
           {
-            label: "会员名称",
+            label: "案件名称",
             prop: "name",
             width: 100
-          },
-          {
-            label: "微信号",
-            prop: "openid",
-            width: 100
-          },
-           {
-            label: "性别",
-            prop: "sex",
-            width: 65
-          },
-          {
-            label: "身份证号",
-            prop: "idCard",
-            width: 100
-          },
-          {
-            label: "参赛次数",
-            prop: "entries",
-            width: 65,
-            // slot:"slot_list_column_entries"
-            statistics:{listIndex:'list_enroll', targetIdKey:'memberId'}
-          },
-           {
-            label: "成绩数",
-            prop: "countAchievement",
-            width: 65,
-            statistics:{listIndex:'list_achievement', targetIdKey:'memberId'}
-          },
-          {
-            label: "唐球积分",
-            prop: "integral",
-            width: 65
-          },       
-          {
-            label: "备注",
-            prop: "remark",
-            width: 65
-          },
-          {
-            label: "职业",
-            prop: "career",
-            width: 50
-          },
-          {
-            label: "球龄",
-            prop: "ballAge",
-            width: 65,
-            formatter: function(rowData) {
-              if (rowData.ballAge == 1) {
-                return "一年以下";
-              } else if (rowData.ballAge == 2) {
-                return "一到三年";
-              } else if (rowData.ballAge == 3) {
-                return "三到五年";
-              } else if (rowData.ballAge == 4) {
-                return "五到十年";
-              } else {
-                return "十年以上";
-              }
-            }
           }
         ],
         //-------筛选表单字段数组-------
         searchFormItems: [
           {
-            label: "会员名称",
+            label: "案件名称",
             prop: "name",
             type: "input_find_vague"
-          },
-          {
-            label: "身份证号",
-            prop: "idCard",
-            type: "input"
           }
         ],
         //-------详情字段数组-------
         detailItems: [
           {
-            label: "会员编号",
+            label: "Id",
             prop: "P1",
           },
           {
-            label: "会员名称",
+            label: "案件名称",
             prop: "name",
-          },
-          {
-            label: "微信号",
-            prop: "openid",
-          },
-          {
-            label: "身份证号",
-            prop: "idCard",
-          },
-          {
-            label: "参赛次数",
-            prop: "entries",
-          },
-          {
-            label: "唐球积分",
-            prop: "integral",
-            width: 65
-          },     
-          
-          {
-            label: "职业",
-            prop: "career",
-            width: 50
-          },
-          {
-            label: "球龄",
-            prop: "ballAge",
-            width: 65,
-            formatter: function(rowData) {
-              if (rowData.ballAge == 1) {
-                return "一年以下";
-              } else if (rowData.ballAge == 2) {
-                return "一到三年";
-              } else if (rowData.ballAge == 3) {
-                return "三到五年";
-              } else if (rowData.ballAge == 4) {
-                return "五到十年";
-              } else {
-                return "十年以上";
-              }
-            }
-          },
-          {
-            label: "备注",
-            prop: "remark",
-            width: 150
-          }  
+          } 
         ],
         //-------新增、修改表单字段数组-------
         formItems: [
          {
-            label: "会员姓名",
+            label: "案件名称",
             prop: "name",
-            width: 100
+       
           },
-          {
-            label: "微信openid",
-            prop: "openid",
-            width: 100
-          },
-          {
-            label: "性别",
-            prop: "sex",
-            type: "select",
-            default:1,
-            options: [{ value: 1, label: "男" }, { value: 2, label: "女" }]
-          },
-          {
-            label: "身份证号",
-            prop: "idCard",
-            width: 100
-          },
-          {
-            label: "备注",
-            prop: "remark",
-            width: 150
+        {
+            label: "案号",
+            prop: "caseId",
           } ,
           {
-            label: "职业",
-            prop: "career",
-            type: "input"
+            label: "描述",
+            prop: "description",
+          } ,
+          {
+            label: "状态",
+            prop: "status",
+            type: "select",
+            default:1,
+            options: [{ value: 1, label: "待立案" }, { value: 2, label: "已立案待保全" }]
           },
           {
-            label: "球龄",
-            prop: "ballAge",
-            type: "select",
-            options: [
-              { label: "一年以下", value: 1 },
-              { label: "一到三年", value: 2 },
-              { label: "三到五年", value: 3 },
-              { label: "五到十年", value: 4 },
-              { label: "十年以上", value: 5 }
-            ]
-          }, 
+            label: "诉讼费",
+            prop: "litigationFee",
+          },
+          {
+            label: "担保费",
+            prop: "guaranteeFee",
+          },
+          {
+            label: "保全费",
+            prop: "insuranceFee",
+          },
+          {
+            label: "原告信息",
+            prop: "plaintiffInfo",
+            default:{},//默认值必须要有，否则新增的时候会出问题
+            cfForm: {
+              formItems: [
+                {
+                  label: "单位名称",
+                  prop: "company"
+                },
+                {
+                  label: "联系人信息",
+                  prop: "contact"
+                }
+              ]
+            }
+          },
+          {
+            label: "被告信息",
+            prop: "defendantInfo",
+            default:{},//默认值必须要有，否则新增的时候会出问题
+            cfForm: {
+              formItems: [
+                {
+                  label: "单位名称",
+                  prop: "company"
+                },
+                {
+                  label: "联系人信息",
+                  prop: "contact"
+                }
+              ]
+            }
+          },
+          {
+            label: "一审信息",
+            prop: "firstInstanceInfo",
+            default:{},//默认值必须要有，否则新增的时候会出问题
+            cfForm: {
+              formItems: [
+                {
+                  label: "单位",
+                  prop: "company"
+                },
+                {
+                  label: "案号",
+                  prop: "caseId"
+                },
+                {
+                  label: "承办人",
+                  prop: "person"
+                },
+                {
+                  label: "联系电话",
+                  prop: "phone"
+                }
+              ]
+            }
+          },
+          {	
+            label: "诉保信息",
+            prop: "guaranteeInfo",
+            default:{},//默认值必须要有，否则新增的时候会出问题
+            cfForm: {
+              formItems: [
+                {
+                  label: "单位",
+                  prop: "company"
+                },
+               
+                {
+                  label: "承办人",
+                  prop: "person"
+                },
+                {
+                  label: "联系电话",
+                  prop: "phone"
+                }
+              ]
+            }
+          },
+          { 	
+            label: "二审信息",
+            prop: "secondInstanceInfo",
+            default:{},//默认值必须要有，否则新增的时候会出问题
+            cfForm: {
+              formItems: [
+                {
+                  label: "单位",
+                  prop: "company"
+                },
+                {
+                  label: "案号",
+                  prop: "caseId"
+                },
+                {
+                  label: "承办人",
+                  prop: "person"
+                },
+                {
+                  label: "联系电话",
+                  prop: "phone"
+                }
+              ]
+            }
+          },
+          {	
+            label: "执行信息",
+            prop: "executionInfo",
+            default:{},//默认值必须要有，否则新增的时候会出问题
+            cfForm: {
+              formItems: [
+                {
+                  label: "单位",
+                  prop: "company"
+                },
+                {
+                  label: "案号",
+                  prop: "caseId"
+                },
+                {
+                  label: "承办人",
+                  prop: "person"
+                },
+                {
+                  label: "联系电话",
+                  prop: "phone"
+                }
+              ]
+            }
+          },
+          		
+	
+	
+	
+	
+	
         ]
       }
     };
