444df6cbc33400c81a2107d8473b4955e98c1189
lyh <113381161@qq.com>
2019-10-17 14:04:11 +0800

    lyh

--- src/assets/js/util.js
+++ src/assets/js/util.js
@@ -1,10 +1,10 @@
 
 window.PUB={}
 
-// window.PUB.domain="http://localhost:3000"
+window.PUB.domain="http://localhost:3000"
  //window.PUB.domain='http://test.dmagic.cn'
 // window.PUB.domain="http://e6234kn.hn3.mofasuidao.cn"//魔法隧道地址
-window.PUB.domain="http://120.76.160.41:3000"
+// window.PUB.domain="http://120.76.160.41:3000"
 // window.PUB.urlUpload=`${PUB.domain}/api_third_part/qiniu_upload?scope=lawyer_case_management`
 window.PUB.urlUpload = `https://up-z2.qiniup.com`//七牛云上传地址（域名）
 window.PUB.urlGetQiniuToken = `${PUB.domain}/api_third_part/get_qiniu_token?scope=lawyer_case_management`
